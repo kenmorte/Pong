71851a3b13e2d13f085322ccbf1483df092f5dca
Christian Morte <cmorte@uci.edu>
2016-03-26 21:30:21 -0700

    Update ball.h

--- ball.h
+++ ball.h
@@ -6,33 +6,6 @@
 #include "point.h"
 #include "pongobject.h"
 
-//# Mobile_Simultons have angles (in radians) and speeds
-//#   (in pixels/update); they inherit locations and
-//#   dimensions (and methods) from Simulton.
-//# These are initialized in __init__ and can be
-//#    queried and set using methods.
-//# Three useful methods help mobile simultons move on their
-//#   canvas, by updating their location and changing their
-//#   angle (bouncing) if they hit a wall.
-
-
-//# The angle unit is radians. If you are unfamiliar with this
-//# unit, examine the diagram below in which all units are in radians.
-//#
-//#                   pi/2
-//#                     |
-//#                     |
-//#                     |
-//#                     |
-//#                     |
-//# pi (or -pi) --------+-------- 0 (or 2pi)
-//#                     |
-//#                     |
-//#                     |
-//#                     |
-//#                     |
-//#                  3pi/2 (or -pi/2)
-
 class Ball: public PongObject, public QGraphicsEllipseItem
 {
 public:
